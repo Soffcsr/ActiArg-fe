fc961acb3e969ee97869d8cddec681fe0256ec3b
Walter Celiz <walterceliz93@gmail.com>
2020-06-07 20:41:12 -0300

    caruselHome 1.1 design

--- src/component/Carousel/ItemCarousel.jsx
+++ src/component/Carousel/ItemCarousel.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 const ItemCarousel = (props) => {
     return(
         <div className={`carousel-item ${props.isActive}`}>
-          <img src={props.img} className="d-block w-100" alt={props.alt} />
+          <img className="carousel-img d-block " src={props.img} alt={props.alt} />
           <div className="carousel-caption">
-            <h5>{props.title}</h5>
-            <p>{props.description}</p>
+            <h5 className="title">{props.title}</h5>
             <button type="button" className="btn btn-primary">{props.nameBtn}</button>
+          </div>
+          <div>
+          <hr/>
+          <p className="carousel-p">{props.description}</p>
           </div>
         </div>
     )
