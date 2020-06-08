2af963fd436f16e1ec2b36babde65de576c91ee0
MelisaGramajo <melisargramajo@gmail.com>
2020-06-08 17:08:10 -0300

    create cards Home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import './component/Carousel/Carousel'
+import './component/Card/CardContent'
 import Carousel from './component/Carousel/Carousel';
+import CardContent from './component/Card/CardContent';
 
 function App() {
   return (
     <div className="App">
       <Carousel />
+      <CardContent />
     </div>
   );
 }
