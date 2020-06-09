eaedf28b4745ca0e9c71f51d77aa9e3492fb5371
Soffcsr <soff.csr@gmail.com>
2020-06-08 21:22:23 -0300

    cambios de estilos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './views/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Home/>
     </div>
   );
 }
