67b77c3cb550f7c0db5635b7227741abafcfda8b
Soffcsr <soff.csr@gmail.com>
2020-06-11 00:25:13 -0300

    footer primer commit

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Navbar from '../component/Navbar'
+import Footer from '../component/Footer'
 import Carousel from '../component/Carousel/Carousel'
 import CardContent from '../component/Card/CardContent'
 
@@ -8,11 +9,11 @@ import CardContent from '../component/Card/CardContent'
 const Home = () => {
     return(
         <div>
-        <Navbar/>
-        <Carousel/>
-        <CardContent/>
+            <Navbar/>
+            <Carousel/>
+            <CardContent/>
+            <Footer/>
         </div>
-
     )
 }
 
