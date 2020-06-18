660de010c4065f8ab2b4b3151dfd301daff257fe
Soffcsr <soff.csr@gmail.com>
2020-06-18 16:29:44 -0300

    add react-router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import './App.css'
 import './component/Carousel/Carousel'
 import Home from './views/Home'
 import GymProfile from './views/GymProfile/GymProfile'
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
 
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/Gym3">
+            <GymProfile/>
+          </Route>
+        </Switch>
+      </Router>
 
-      <GymProfile/>
 
     </div>
   )
