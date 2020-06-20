2d06973833df67c72e49f7fe5b04163c6c022112
Walter Celiz <walterceliz93@gmail.com>
2020-06-20 15:57:43 -0300

    Price component terminado, falta arrelgar el choque de css

--- src/views/GymProfile/GymProfile.jsx
+++ src/views/GymProfile/GymProfile.jsx
@@ -1,43 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DatasJumbotron from '../../samples/DatasJumbotron.json'
 import DataContact from '../../samples/DataContact.json'
 import Jumbotron from '../GymProfile/Jumbotron/Jumbotron'
 import Contact from '../GymProfile/Contact'
 import Activity from './Activities/Activity'
 import DataActivities from '../../samples/DataActivities.json'
+import Price from './Price/Price'
+import DataPrices from '../../samples/DataPrices.json'
 
+const GymProfile = () => {
+    const [isActive, setActive] = useState(false)
 
-const GymProfile = props => {
-    return(
+    const toggle = () => {
+        setActive(!isActive)
+    }
+
+    return (
         <div>
-        {DatasJumbotron.map((data) => {
-            return <Jumbotron
-                img1={data.img1}
-                img2={data.img2}
-                img3={data.img3}
-                title={data.title}
-                subtitle={data.subtitle}
-                description={data.description}
-                objective1={data.objective1} 
-                objectivedescription1={data.objectivedescription1} 
-                objective2={data.objective2} 
-                objectivedescription2={data.objectivedescription2} 
-                objective3={data.objective3} 
-                objectivedescription3={data.objectivedescription3} 
-                
+            {DatasJumbotron.map((data) => {
+                return <Jumbotron
+                    img1={data.img1}
+                    img2={data.img2}
+                    img3={data.img3}
+                    title={data.title}
+                    subtitle={data.subtitle}
+                    description={data.description}
+                    objective1={data.objective1}
+                    objectivedescription1={data.objectivedescription1}
+                    objective2={data.objective2}
+                    objectivedescription2={data.objectivedescription2}
+                    objective3={data.objective3}
+                    objectivedescription3={data.objectivedescription3}
+
                 />
-               
-        })}
-    <br/>
-    {DataContact.map((data) => {
-            return <Contact 
-            phone= {data.phone}
-            adress={data.adress}
-            atention={data.atention}            
-            />  
-    })}
-        
-        <div className="card-group">
+
+            })}
+            <br />
+            {DataContact.map((data) => {
+                return <Contact
+                    phone={data.phone}
+                    adress={data.adress}
+                    atention={data.atention}
+                />
+            })}
+
+            <div className="card-group">
                 {DataActivities.map((DataActivity) => {
                     return <Activity
                         key={DataActivity.id}
@@ -51,7 +58,33 @@ const GymProfile = props => {
                     />
                 })}
             </div>
-    </div>
+
+            <div className="customPrice"
+                style={{
+                    backgroundImage: "url(" + "https://i.ibb.co/0jfhcrT/price-bg.jpg" + ")"
+                }}>
+                <h2 className="customPriceH2">Nuestros precios</h2>
+
+                <button className="customBtn" onClick={toggle}>
+                    {isActive ? 'MES' : 'CLASE'}
+                </button>
+
+                <div className="card-group">
+                    {DataPrices.map((DataPrice) => {
+                        return <Price
+                            title={DataPrice.title}
+                            price={
+                                isActive
+                                    ? DataPrice.price * 20
+                                    : DataPrice.price
+                            }
+                            description={DataPrice.description}
+                        />
+                    })}
+                </div>
+            </div>
+
+        </div>
 
     )
 }
