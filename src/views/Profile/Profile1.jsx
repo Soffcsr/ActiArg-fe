cf5c50f25bb50f53e6862c231c96b35eadad8408
Soffcsr <soff.csr@gmail.com>
2020-07-22 22:11:43 -0300

    hide price table if not loged user

--- src/views/Profile/Profile1.jsx
+++ src/views/Profile/Profile1.jsx
@@ -11,6 +11,7 @@ import Price from './Price'
 import Contact from './Contact'
 import Footer from '../../component/Footer'
 import ScrollArrow from '../../component/ScrollArrow'
+import Alert from '../../component/Alert'
 import '../../App.css'
 import { showActivities, showTurns, showCarousels, showPublicits, findGymById } from '../../actions'
 
@@ -53,7 +54,9 @@ const Profile1 = (props) => {
                 left={['Home', 'Gimnasios']}
                 right={['Nosotros', 'Contactanos']}
             />
-
+            {
+                !sessionStorage.getItem('token') ? <Alert message="Para ver nuestros precios, por favor logueate"/> : null
+            }
             <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
                 <ol id="myCarousel-indicators" className="carousel-indicators">
                     <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
@@ -119,105 +122,108 @@ const Profile1 = (props) => {
                     />
                 })}
             </div>
-                {/* turnbygym */}
-            <div className="classtime-section class-time-table spad table-responsive">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-lg-12 text-center">
-                            <div className="section-title">
-                                <h2>Horarios</h2>
+                {/* turnbygym */ }
+                {
+                    sessionStorage.getItem('token') ? 
+                    <div className="classtime-section class-time-table spad table-responsive">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-12 col-lg-12 text-center">
+                                    <div className="section-title">
+                                        <h2>Horarios</h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="classtime-table">
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th></th>
+                                            <th>Lunes</th>
+                                            <th>Martes</th>
+                                            <th>Miercoles</th>
+                                            <th>Jueves</th>
+                                            <th>Viernes</th>
+                                            <th>Sabado</th>
+                                            <th>Domingo</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+        
+                                        { 
+                                            props.turns ?
+                                                props.turns.map((turn) => {
+                                                    console.log("turn::", turn)
+                                                    return <ItemsTable
+                                                        idgym = {id}
+                                                        workoutTime={turn.workoutTime}
+                                                        idMonday={turn.days[0]._id}
+                                                        mondayHourClass={turn.days[0].HourClass}
+                                                        mondayNameClass={turn.days[0].NameClass}
+                                                        mondayPartialPlaces={turn.days[0].PartialPlaces}
+                                                        mondayTotallPlaces={turn.days[0].TotallPlaces}
+                                                        mondayAction={turn.days[0].Action}
+                                                        mondayNameBtn={turn.days[0].NameBtn}
+        
+                                                        idTuesday={turn.days[1]._id}
+                                                        tuesdayHourClass={turn.days[1].HourClass}
+                                                        tuesdayNameClass={turn.days[1].NameClass}
+                                                        tuesdayPartialPlaces={turn.days[1].PartialPlaces}
+                                                        tuesdayTotallPlaces={turn.days[1].TotallPlaces}
+                                                        tuesdayAction={turn.days[1].Action}
+                                                        tuesdayNameBtn={turn.days[1].NameBtn}
+        
+                                                        idWednesday={turn.days[2]._id}
+                                                        wednesdayHourClass={turn.days[2].HourClass}
+                                                        wednesdayNameClass={turn.days[2].NameClass}
+                                                        wednesdayPartialPlaces={turn.days[2].PartialPlaces}
+                                                        wednesdayTotallPlaces={turn.days[2].TotallPlaces}
+                                                        wednesdayAction={turn.days[2].Action}
+                                                        wednesdayNameBtn={turn.days[2].NameBtn}
+        
+                                                        idThursday ={turn.days[3]._id}
+                                                        thursdayHourClass={turn.days[3].HourClass}
+                                                        thursdayNameClass={turn.days[3].NameClass}
+                                                        thursdayPartialPlaces={turn.days[3].PartialPlaces}
+                                                        thursdayTotallPlaces={turn.days[3].TotallPlaces}
+                                                        thursdayAction={turn.days[3].Action}
+                                                        thursdayNameBtn={turn.days[3].NameBtn}
+        
+                                                        idFriday={turn.days[4]._id}
+                                                        fridayHourClass={turn.days[4].HourClass}
+                                                        fridayNameClass={turn.days[4].NameClass}
+                                                        fridayPartialPlaces={turn.days[4].PartialPlaces}
+                                                        fridayTotallPlaces={turn.days[4].TotallPlaces}
+                                                        fridayAction={turn.days[4].Action}
+                                                        fridayNameBtn={turn.days[4].NameBtn}
+        
+                                                        idSaturday = {turn.days[5]._id}
+                                                        saturdayHourClass={turn.days[5].HourClass}
+                                                        saturdayNameClass={turn.days[5].NameClass}
+                                                        saturdayPartialPlaces={turn.days[5].PartialPlaces}
+                                                        saturdayTotallPlaces={turn.days[5].TotallPlaces}
+                                                        saturdayAction={turn.days[5].Action}
+                                                        saturdayNameBtn={turn.days[5].NameBtn}
+        
+                                                        idSunday={turn.days[6]._id}
+                                                        sundayHourClass={turn.days[6].HourClass}
+                                                        sundayNameClass={turn.days[6].NameClass}
+                                                        sundayPartialPlaces={turn.days[6].PartialPlaces}
+                                                        sundayTotallPlaces={turn.days[6].TotallPlaces}
+                                                        sundayAction={turn.days[6].Action}
+                                                        sundayNameBtn={turn.days[6].NameBtn}
+                                                    />
+                                                })
+                                            : null
+                                            }
+        
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
-
-                    <div className="classtime-table">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>Lunes</th>
-                                    <th>Martes</th>
-                                    <th>Miercoles</th>
-                                    <th>Jueves</th>
-                                    <th>Viernes</th>
-                                    <th>Sabado</th>
-                                    <th>Domingo</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-
-                                { 
-                                    props.turns ?
-                                        props.turns.map((turn) => {
-                                            console.log("turn::", turn)
-                                            return <ItemsTable
-                                                idgym = {id}
-                                                workoutTime={turn.workoutTime}
-                                                idMonday={turn.days[0]._id}
-                                                mondayHourClass={turn.days[0].HourClass}
-                                                mondayNameClass={turn.days[0].NameClass}
-                                                mondayPartialPlaces={turn.days[0].PartialPlaces}
-                                                mondayTotallPlaces={turn.days[0].TotallPlaces}
-                                                mondayAction={turn.days[0].Action}
-                                                mondayNameBtn={turn.days[0].NameBtn}
-
-                                                idTuesday={turn.days[1]._id}
-                                                tuesdayHourClass={turn.days[1].HourClass}
-                                                tuesdayNameClass={turn.days[1].NameClass}
-                                                tuesdayPartialPlaces={turn.days[1].PartialPlaces}
-                                                tuesdayTotallPlaces={turn.days[1].TotallPlaces}
-                                                tuesdayAction={turn.days[1].Action}
-                                                tuesdayNameBtn={turn.days[1].NameBtn}
-
-                                                idWednesday={turn.days[2]._id}
-                                                wednesdayHourClass={turn.days[2].HourClass}
-                                                wednesdayNameClass={turn.days[2].NameClass}
-                                                wednesdayPartialPlaces={turn.days[2].PartialPlaces}
-                                                wednesdayTotallPlaces={turn.days[2].TotallPlaces}
-                                                wednesdayAction={turn.days[2].Action}
-                                                wednesdayNameBtn={turn.days[2].NameBtn}
-
-                                                idThursday ={turn.days[3]._id}
-                                                thursdayHourClass={turn.days[3].HourClass}
-                                                thursdayNameClass={turn.days[3].NameClass}
-                                                thursdayPartialPlaces={turn.days[3].PartialPlaces}
-                                                thursdayTotallPlaces={turn.days[3].TotallPlaces}
-                                                thursdayAction={turn.days[3].Action}
-                                                thursdayNameBtn={turn.days[3].NameBtn}
-
-                                                idFriday={turn.days[4]._id}
-                                                fridayHourClass={turn.days[4].HourClass}
-                                                fridayNameClass={turn.days[4].NameClass}
-                                                fridayPartialPlaces={turn.days[4].PartialPlaces}
-                                                fridayTotallPlaces={turn.days[4].TotallPlaces}
-                                                fridayAction={turn.days[4].Action}
-                                                fridayNameBtn={turn.days[4].NameBtn}
-
-                                                idSaturday = {turn.days[5]._id}
-                                                saturdayHourClass={turn.days[5].HourClass}
-                                                saturdayNameClass={turn.days[5].NameClass}
-                                                saturdayPartialPlaces={turn.days[5].PartialPlaces}
-                                                saturdayTotallPlaces={turn.days[5].TotallPlaces}
-                                                saturdayAction={turn.days[5].Action}
-                                                saturdayNameBtn={turn.days[5].NameBtn}
-
-                                                idSunday={turn.days[6]._id}
-                                                sundayHourClass={turn.days[6].HourClass}
-                                                sundayNameClass={turn.days[6].NameClass}
-                                                sundayPartialPlaces={turn.days[6].PartialPlaces}
-                                                sundayTotallPlaces={turn.days[6].TotallPlaces}
-                                                sundayAction={turn.days[6].Action}
-                                                sundayNameBtn={turn.days[6].NameBtn}
-                                            />
-                                        })
-                                    : null
-                                    }
-
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+                    : null
+                }
             {/* activitybyGym */}
             <div className="customPrice"
                 style={{
